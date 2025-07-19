22cde7ce1601e6c6c85b2cca3d1fd7e42dd946c0
Asur1212 <satusaw8@gmail.com>
2025-07-19 23:30:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 // Middleware
 app.use(cors());
